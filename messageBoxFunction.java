c884e9d16f280adcf16b3466e7898e1f01649838
ANTONINEUTRON <antonineutron@gmail.com>
2020-04-11 20:08:27 +0100

    Delete messageBoxFunction.java

--- messageBoxFunction.java
+++
@@ -1,9 +0,0 @@
-public void messageBox(String method, String message){
-      Log.d("Exception: "+method, message);
-      AlertDialog.Builder messageBox = new AlertDialog.Builder(this);
-      messageBox.setTitle(method);
-      messageBox.setMessage(message);
-      messageBox.setCancelable(false);
-      messageBox.setNeutralButton("Ok",null);
-      messageBox.show();
-    }
